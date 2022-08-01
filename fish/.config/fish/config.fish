9ea9ef73e7f4faa2c9f71183ccd73fb5f744c599
Kieran O'Brien <kieran@obrien.id>
2022-08-02 00:25:19 +1000

    Update fish paths and env vars

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,21 @@
 if status is-interactive
+    set -gx GPG_TTY (tty)
+
+    set -gx ANDROID_HOME ~/Library/Android/sdk
+
+    set -gx HOMEBREW_PREFIX /opt/homebrew
+    set -gx HOMEBREW_CELLAR {$HOMEBREW_PREFIX}/Cellar
+    set -gx HOMEBREW_REPOSITORY $HOMEBREW_PREFIX
+    set -gx HOMEBREW_NO_ANALYTICS 1
+
+    fish_add_path -g ~/.local/bin "$HOMEBREW_PREFIX"/{bin,sbin} "$ANDROID_HOME"/platform-tools
+
+    set -gx LESS --IGNORE-CASE --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD --status-column --tabs=4 --window=-4
+  
+    type -q vivid && set -gx LS_COLORS (vivid generate jellybeans)
+    
     fish_config theme choose "Tomorrow Night"
-    set -gx LS_COLORS "$(vivid generate jellybeans)"
+
     set -g fish_greeting
 
     set -U tide_character_icon \u276f
