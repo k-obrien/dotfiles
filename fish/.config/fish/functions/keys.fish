8bbe3d72f844ee790248ce54f6ac9f9674d202b0
Kieran O'Brien <kieran.o'brien@medibank.com.au>
2024-03-18 18:03:54 +1100

    Update key shortcut reference

--- fish/.config/fish/functions/keys.fish
+++ fish/.config/fish/functions/keys.fish
@@ -1,28 +1,33 @@
 function keys
-echo "\
-⌥B: Belay current command
-⌥S: Prepend sudo to current or previous command
-⌥E: Edit command in editor
-⌥C: Capitalise first letter of word under cursor
-⌥U: Capitalise word under cursor
-^T: Transpose current and previous character
-⌥T: Tranpose current and previous word
-^Z: Undo most recent line edit
-⌥/: Revert most recent undo of line edit
+   function decorate
+      set_color --bold blue; printf $argv[1]; set_color normal; printf ": "; set_color --italics; echo $argv[2]
+   end
 
-^[AE]: Move cursor to start/end of line
-⌥[←→]: Move cursor one inclusive word OR navigate directory stack
-⇧[←→]: Move cursor one exclusive word OR accept big word of autosuggestion
+   decorate "   ⌥B" "Belay current command"
+   decorate "   ⌥S" "Prepend sudo to current or previous command"
+   decorate "   ⌥E" "Edit command in editor"
+   decorate "   ⌥C" "Capitalise first letter of word under cursor"
+   decorate "   ⌥U" "Capitalise word under cursor"
+   decorate "   ^T" "Transpose current and previous character"
+   decorate "   ⌥T" "Tranpose current and previous word"
+   decorate "   ^Z" "Undo most recent line edit"
+   decorate "   ⌥/" "Revert most recent undo of line edit"
+   echo
+   decorate "^[AE]" "Move cursor to start/end of line"
+   decorate "⌥[←→]" "Move cursor one inclusive word OR navigate directory stack"
+   decorate "⇧[←→]" "Move cursor one exclusive word OR accept big word of autosuggestion"
+   echo
+   decorate "   ⌥W" "Show short description of app under cursor"
+   decorate "   ⌥H" "Show manpage for app under cursor"
+   decorate "   ⌥L" "List contents of directory under cursor or current directory"
+   echo
+   decorate "   ^R" "Search command history"
+   decorate "⌥[↑↓]" "Search command history for instances of original token under cursor"
+   decorate "   ^V" "Search environment variables"
+   decorate "  ^⌥F" "Search current directory"
+   decorate "  ^⌥L" "Search git log"
+   decorate "  ^⌥S" "Search git status"
+   decorate "  ^⌥P" "Search processes"
 
-⌥W: Show short description of app under cursor
-⌥H: Show manpage for app under cursor
-⌥L: List contents of directory under cursor or current directory
-
-   ^R: Search command history
-⌥[↑↓]: Search command history for instances of original token under cursor
-   ^V: Search environment variables
-  ^⌥F: Search current directory
-  ^⌥L: Search git log
-  ^⌥S: Search git status
-  ^⌥P: Search processes"
+   functions --erase decorate
 end
