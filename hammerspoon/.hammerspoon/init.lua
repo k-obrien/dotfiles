7ebf3f38684e7f163359025c05e86586f1d22669
Kieran O'Brien <kieran.o'brien@medibank.com.au>
2024-03-20 13:26:23 +1100

    Update phonetic alphabet helper

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -116,33 +116,8 @@ end
 
 -- >>> Show Phonetic Alphabet
 local function showPhoneticAlphabet()
-    local alphabet = "A\tAlpha\t\t\t" ..
-                     "N\tNovember\n" ..
-                     "B\tBravo\t\t\t" ..
-                     "O\tOscar\n" ..
-                     "C\tCharlie\t\t" ..
-                     "P\tPapa\n" ..
-                     "D\tDelta\t\t\t" ..
-                     "Q\tQuebec\n" ..
-                     "E\tEcho\t\t\t" ..
-                     "R\tRomeo\n" ..
-                     "F\tFoxtrot\t\t" ..
-                     "S\tSierra\n" ..
-                     "G\tGolf\t\t\t" ..
-                     "T\tTango\n" ..
-                     "H\tHotel\t\t\t" ..
-                     "U\tUniform\n" ..
-                     "I\tIndia\t\t\t" ..
-                     "V\tVictor\n" ..
-                     "J\tJuliet\t\t" ..
-                     "W\tWhisky\n" ..
-                     "K\tKilo\t\t\t" ..
-                     "X\tX-Ray\n" ..
-                     "L\tLima\t\t\t" ..
-                     "Y\tYankee\n" ..
-                     "M\tMike\t\t\t" ..
-                     "Z\tZulu\n"
-    hs.alert.show(alphabet:sub(1, -2), "infinite")
+    local alphabet = hs.styledtext.getStyledTextFromFile("phonetic-alphabet.html", "html")
+    hs.alert.show(alphabet, { atScreenEdge = 0 }, "infinite")
 end
 
 hs.hotkey.bind(modifier, "p", "Phonetic Alphabet", showPhoneticAlphabet, hs.alert.closeAll)
