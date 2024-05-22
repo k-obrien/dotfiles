874c5e50dce23eba314738bcebae87032e7d70fa
Kieran O'Brien <kieran.o'brien@medibank.com.au>
2024-05-23 09:55:11 +1000

    Update gh scripts

---
+++ binaries/.local/bin/show-open-involved-pull-requests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Show Involved Pull Requests
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon https://git-scm.com/images/logos/downloads/Git-Icon-Black.png
+# @raycast.iconDark https://git-scm.com/images/logos/downloads/Git-Icon-White.png
+# @raycast.argument1 { "type": "text", "placeholder": "organisation/repository" }
+
+# shellcheck source-path=SCRIPTDIR
+source "${0%/*}/common"
+
+_depend_on gh
+
+gh review-open-involved-prs "${1:?repository required}"
