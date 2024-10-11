a551c29ccda775a9946a18557922a29daaea9381
Kieran O'Brien <kieran.o'brien@medibank.com.au>
2024-10-11 11:54:55 +1100

    Update gh PR shortcuts

--- binaries/.local/bin/my-pull-requests.sh
+++ binaries/.local/bin/my-pull-requests.sh
@@ -2,17 +2,16 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Show Open Pull Requests
+# @raycast.title My Pull Requests
 # @raycast.mode fullOutput
 
 # Optional parameters:
 # @raycast.icon https://git-scm.com/images/logos/downloads/Git-Icon-Black.png
 # @raycast.iconDark https://git-scm.com/images/logos/downloads/Git-Icon-White.png
-# @raycast.argument1 { "type": "text", "placeholder": "organisation/repository" }
 
 # shellcheck source-path=SCRIPTDIR
 source "${0%/*}/common"
 
 _depend_on gh
 
-gh review-open-prs "${1:?repository required}"
+gh my-prs
--- binaries/.local/bin/reviewed-pull-requests.sh
+++ binaries/.local/bin/reviewed-pull-requests.sh
@@ -2,16 +2,17 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Show My Open Pull Requests
+# @raycast.title Reviewed Pull Requests
 # @raycast.mode fullOutput
 
 # Optional parameters:
 # @raycast.icon https://git-scm.com/images/logos/downloads/Git-Icon-Black.png
 # @raycast.iconDark https://git-scm.com/images/logos/downloads/Git-Icon-White.png
+# @raycast.argument1 { "type": "text", "placeholder": "organisation/repository" }
 
 # shellcheck source-path=SCRIPTDIR
 source "${0%/*}/common"
 
 _depend_on gh
 
-gh review-my-open-prs
+gh reviewed-prs "${1:?repository required}"
