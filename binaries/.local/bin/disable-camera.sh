c17040b936b346fa2bb22e2a310834297f5c511c
Kieran O'Brien <kieran.obrien@medibank.com.au>
2025-02-06 13:20:34 +1100

    Revert "Add camera management profile and scripts"
    This reverts commit 337fbb420d6c1e6d055f9e2b08ea4788096c652f.

--- binaries/.local/bin/disable-camera.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck source-path=SCRIPTDIR
-source "${0%/*}/common"
-
-_depend_on open
-
-open "x-apple.systempreferences:com.apple.Profiles-Settings.extension"
-open ~/.config/profiles/disable-camera.mobileconfig
--- binaries/.local/bin/enable-camera.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck source-path=SCRIPTDIR
-source "${0%/*}/common"
-
-_depend_on profiles
-
-profiles -R -p id.obrien.disablecamera
